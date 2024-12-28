94314e0f575bb0af75a30516f5f97adfc662020e
Ha-Ba <133162407+Ha-Ba@users.noreply.github.com>
2024-12-28 23:29:55 +0100

    JS close menu, styles, texts

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const menuList = document.querySelector("nav");
 const hamburgerIcon = document.querySelector(".fa-solid");
 
 menuIcon.addEventListener("click", () => {
-  // console.log("Klikli jste");
   if (hamburgerIcon.classList[1] === "fa-bars") {
     hamburgerIcon.classList.add("fa-xmark");
     hamburgerIcon.classList.remove("fa-bars");
@@ -12,5 +11,15 @@ menuIcon.addEventListener("click", () => {
     hamburgerIcon.classList.add("fa-bars");
     hamburgerIcon.classList.remove("fa-xmark");
     menuList.style.display = "none";
+  }
+});
+
+menuList.addEventListener("click", () => {
+  if (hamburgerIcon.classList[1] === "fa-xmark") {
+    hamburgerIcon.classList.remove("fa-xmark");
+    hamburgerIcon.classList.add("fa-bars");
+    menuList.style.display = "none";
+  } else {
+    return
   }
 });
